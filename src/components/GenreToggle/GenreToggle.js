ba38e159eb858e962898d6816928c148d8daf133
David Verweij <davidverweij@gmail.com>
2022-04-22 11:43:42 +0100

    finalise interactive elements with some basic CSS

--- src/components/GenreToggle/GenreToggle.js
+++ src/components/GenreToggle/GenreToggle.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./GenreToggle.module.css";
 
 const GenreToggle = (props) => {
+  const [showGenreState, setShowGenreState] = useState(false);
+
+  const showGenreHandler = () => {
+    setShowGenreState(!showGenreState);
+  };
+
   return (
     <div className={styles.genre}>
-      <div className={styles.dropdown}>
-        {props.genres.map((genre) => {
-          return (
-            <div className={styles.option} key={genre}>
-              <input
-                className={styles.checkbox}
-                type="checkbox"
-                id={genre}
-                name={genre}
-              ></input>
-              <label htmlFor={genre}>{genre}</label>
-            </div>
-          );
-        })}
+      <div className={styles.button}>
+        <div onClick={showGenreHandler}>
+          <span className={showGenreState ? styles.open : ""}>
+            Select Genre
+          </span>
+        </div>
+        <div
+          className={`${!showGenreState ? styles.hide : ""} ${styles.dropdown}`}
+        >
+          {props.genres.map((genre) => {
+            return (
+              <div className={styles.option} key={genre}>
+                <input
+                  className={styles.checkbox}
+                  type="checkbox"
+                  id={genre}
+                  name={genre}
+                ></input>
+                <label htmlFor={genre}>{genre}</label>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
