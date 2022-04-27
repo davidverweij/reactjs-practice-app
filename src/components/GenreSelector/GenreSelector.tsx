8113107de20732e0a33637dd3530ab16ff78f8f6
David Verweij <davidverweij@gmail.com>
2022-04-27 14:26:27 +0100

    improve error boundary setup

--- src/components/GenreSelector/GenreSelector.tsx
+++ src/components/GenreSelector/GenreSelector.tsx
@@ -7,16 +7,11 @@ interface GenreSelectorProps {
 }
 
 const GenreSelector = ({ genres }: GenreSelectorProps): JSX.Element => {
-  // Why is the error-boundary not invoked?
-  const fakeError = () => {
-    throw new Error("I crashed!");
-  };
-
   return (
     <div className={styles.genres}>
       {genres.map((genre, index) => (
         <div className={index == 0 ? styles.selected : ""} key={genre}>
-          <span onClick={fakeError}>{genre}</span>
+          <span>{genre}</span>
         </div>
       ))}
     </div>
--- src/containers/ErrorBoundary/ErrorBoundary.tsx
+++ src/containers/ErrorBoundary/ErrorBoundary.tsx
@@ -1,33 +1,40 @@
-import React from "react";
+import React, { Component, ReactNode, ErrorInfo } from "react";
+
+import styles from "./ErrorBoundary.module.scss";
 
 interface ErrorBoundaryProps {
-  children?: any;
+  children?: ReactNode;
 }
 
 interface ErrorBoundaryState {
   hasError: boolean;
 }
 
-class ErrorBoundary extends React.Component<
-  ErrorBoundaryProps,
-  ErrorBoundaryState
-> {
+const ErrorComponent = (): JSX.Element => {
+  return (
+    <div className={styles["error-container"]}>
+      Something went wrong. Please bear with us as we fix the issue.
+    </div>
+  );
+};
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   public state: ErrorBoundaryState = {
     hasError: false,
   };
 
-  componentDidCatch() {
-    this.setState({
-      hasError: true,
-    });
+  public static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
   }
 
-  private ErrorMessage =
-    "Something went wrong. Please bear with us as we fix the issue.";
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
 
   public render() {
     if (this.state.hasError) {
-      return this.ErrorMessage;
+      return <ErrorComponent />;
     }
     return this.props.children;
   }
